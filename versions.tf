c934188533e3c47c73b5066f12d73a79d8384363
KDDevGIT <kymasterbox@gmail.com>
2025-09-01 17:54:07 -0700

    added versions.tf

---
+++ versions.tf
@@ -0,0 +1,10 @@
+# Terraform Versions
+terraform {
+    required_version = ">= 1.5.0"
+    required_providers {
+        libvirt = {
+            source = "dmacvicar/libvirt"
+            version = "~> 0.7"
+        }
+    }
+}
